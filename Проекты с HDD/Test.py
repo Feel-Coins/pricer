57b280c3f69595acdaa5590aedff0605d76b4eca
Feel_Coins <vladislav4ik.sib@gmail.com>
2022-03-07 19:38:20 +0700

    Bug fixed: -num with 0 in input has not counting in result -0 in input not move all result to left (not add 0 in result)

--- Проекты с HDD/Test.py
+++ Проекты с HDD/Test.py
@@ -2,14 +2,18 @@ number = int(input('input the number: '))
 len_numb = len(str(number))
 math = []
 for i in str(number):
-    len_numb -= 1
-    i += "0" * len_numb
-    math.append(i)
+    if i == "0":
+        len_numb -= 1
+        continue
+    else:
+        len_numb -= 1
+        i += "0" * len_numb
+        math.append(i)
 
 count = ''
 for i in math:
     count += i
-    if math.index(i) != len(str(number))-1:
+    if math.index(i) != len(math)-1:
         count += ' + '
 
 print(count)
