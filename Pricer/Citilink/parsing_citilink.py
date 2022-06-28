2dbe14307b9aac25b45151b65bc3643ee9433401
Feel-Coins <feel.coins@yandex.ru>
2022-06-29 00:02:29 +0700

    #parcing citilink all products and all pages# *Trouble with prices in "moduli-pamyati": Prices doubling..

--- Pricer/Citilink/parsing_citilink.py
+++ Pricer/Citilink/parsing_citilink.py
@@ -1,13 +1,19 @@
 # scraper for citilink.ru
 import requests
+import re
 import json
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 page = 1
+device = ['moduli-pamyati']
+device_counter = 0
+data_page = 0
+# 'processory', 'videokarty', 'materinskie-platy', , 'bloki-pitaniya', 'korpusa'
 
 
-def get_data(city_code = 'nvs_cl%3A'):
+def get_data(city_code='nvs_cl%3A'):
+    global data_page, device_counter, device
     ua = UserAgent()
 
     headers = {
@@ -20,7 +26,8 @@ def get_data(city_code = 'nvs_cl%3A'):
         '_space': f'{city_code}'
     }
 
-    response = requests.get(url=f'https://www.citilink.ru/catalog/videokarty/?p={page}', headers=headers, cookies=cookies)
+    response = requests.get(url=f'https://www.citilink.ru/catalog/{device[device_counter]}/?p={page}', headers=headers,
+                            cookies=cookies)
 
     with open(f'data from citilinks page {page}.json', 'w') as file:
         file.write(response.text)
@@ -29,11 +36,26 @@ def get_data(city_code = 'nvs_cl%3A'):
         src = file.read()
 
     soup = BeautifulSoup(src, 'lxml')
-    cards = soup.find_all('a', class_='ProductCardHorizontal__title')
-    current_prices = soup.find_all('span', class_='ProductCardHorizontal__price_current-price')
+    if device[device_counter] == 'moduli-pamyati':
+        cards = soup.find_all('a', class_='ProductCardVertical__name')
+    else:
+        cards = soup.find_all('a', class_='ProductCardHorizontal__title')
 
+    if device[device_counter] == 'moduli-pamyati':
+        current_prices = soup.find_all('span', class_='ProductCardVerticalPrice__price-current_current-price '
+                                                      'js--ProductCardVerticalPrice__price-current_current-price')
+    else:
+        current_prices = soup.find_all('span', class_='ProductCardHorizontal__price_current-price')
+
+    num_of_pages = soup.find_all('a', class_=(re.compile('PaginationWidget__page js--PaginationWidget__page')))
     cards = list(cards)
     current_prices = list(current_prices)
+
+    if data_page == 1:
+        data_page = len(num_of_pages)
+    else:
+        pass
+    print(f'In {device[device_counter]} citilink have {data_page} pages')
 
     card_list = []
     price_list = []
@@ -41,24 +63,35 @@ def get_data(city_code = 'nvs_cl%3A'):
     for card in cards:
         card = card.text.strip()
         card_list.append(card)
-    for price in current_prices:
-        price = price.text.strip()
-        price_list.append(price)
 
-    print(f'Page {page} is ready. Parsed titles and prices: ')
+    for price in current_prices:
+        if device[device_counter] == 'moduli-pamyati':
+            price = price.text.strip()
+            if price in price_list:
+                price_list.append(price)
+        else:
+            price = price.text.strip()
+            price_list.append(price)
+
+    print(f'Page {page} is ready. Parsed {device[device_counter]} and their prices \nPlease stand by...')
     print(len(card_list), len(price_list))
 
     result = dict(zip(card_list, price_list))
 
-    with open(f'product card citilinks page {page}.json', 'w') as file:
+    with open(f' citilinks {device[device_counter]}.json', 'a') as file:
         json.dump(result, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    global page
-    while page <= 3:
-        get_data()
-        page += 1
+    global page, device_counter, data_page
+
+    while device_counter != len(device):
+        data_page = page = 1
+        while page <= data_page:
+            get_data()
+            page += 1
+        device_counter += 1
+    print('Parcing complete!')
 
 
 if __name__ == '__main__':
