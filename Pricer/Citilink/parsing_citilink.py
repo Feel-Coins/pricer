25614950a399b0fd903cc0ade46793accdfcf4df
Feel-Coins <feel.coins@yandex.ru>
2022-06-28 16:20:36 +0700

    #parcing citilink v-cards 3 pages# *Done

--- Pricer/Citilink/parsing_citilink.py
+++ Pricer/Citilink/parsing_citilink.py
@@ -4,12 +4,15 @@ import json
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
+page = 1
+
 
 def get_data(city_code = 'nvs_cl%3A'):
     ua = UserAgent()
 
     headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3;q=0.9',
         'User-Agent': ua.random
     }
     #change cookies "'_space': 'nvs_cl%3A'" to change city
@@ -17,12 +20,12 @@ def get_data(city_code = 'nvs_cl%3A'):
         '_space': f'{city_code}'
     }
 
-    response = requests.get(url='https://www.citilink.ru/catalog/videokarty/', headers=headers, cookies=cookies)
+    response = requests.get(url=f'https://www.citilink.ru/catalog/videokarty/?p={page}', headers=headers, cookies=cookies)
 
-    # with open('data.json', 'w') as file:
-    #     file.write(response.text)
+    with open(f'data from citilinks page {page}.json', 'w') as file:
+        file.write(response.text)
 
-    with open('data.json') as file:
+    with open(f'data from citilinks page {page}.json') as file:
         src = file.read()
 
     soup = BeautifulSoup(src, 'lxml')
@@ -31,10 +34,6 @@ def get_data(city_code = 'nvs_cl%3A'):
 
     cards = list(cards)
     current_prices = list(current_prices)
-    print(cards)
-    print(current_prices)
-    print(len(cards))
-    print(len(current_prices))
 
     card_list = []
     price_list = []
@@ -46,15 +45,20 @@ def get_data(city_code = 'nvs_cl%3A'):
         price = price.text.strip()
         price_list.append(price)
 
+    print(f'Page {page} is ready. Parsed titles and prices: ')
     print(len(card_list), len(price_list))
+
     result = dict(zip(card_list, price_list))
 
-    with open('result.json', 'w') as file:
+    with open(f'product card citilinks page {page}.json', 'w') as file:
         json.dump(result, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    get_data()
+    global page
+    while page <= 3:
+        get_data()
+        page += 1
 
 
 if __name__ == '__main__':
