e2539422405c162e918c2f1d2fbafba9197c4d13
Feel-Coins <feel.coins@yandex.ru>
2022-07-05 12:42:17 +0700

    #added links on product page and fixing bug with page counting#
    NEXT STEP: pull program in tg-bot

--- Pricer/Citilink/parsing_citilink.py
+++ Pricer/Citilink/parsing_citilink.py
@@ -8,18 +8,19 @@ import psycopg2
 import datetime
 
 page = 1
+# 'processory', 'videokarty', 'materinskie-platy', , 'bloki-pitaniya', 'korpusa'
 device = ['processory', 'videokarty', 'materinskie-platy', 'moduli-pamyati', 'bloki-pitaniya', 'korpusa']
 device_counter = 0
 data_page = 0
-# 'processory', 'videokarty', 'materinskie-platy', , 'bloki-pitaniya', 'korpusa'
+
 
 # creating database
 date_string = datetime.datetime.now()
 now = str(date_string.strftime('%d%m%y%H%M'))
 connection = psycopg2.connect('dbname=Pricer user=postgres')
 cursor = connection.cursor()
-cursor.execute(f'create table '
-               f'citilink_{now}(id serial primary key, class varchar, product_name varchar, price varchar, link varchar);')
+cursor.execute(f'create table citilink_{now}'
+               f'(id serial primary key, class varchar, product_name varchar, price integer, link varchar);')
 connection.commit()
 
 
@@ -59,55 +60,63 @@ def get_data(city_code='nvs_cl%3A'):
                                                       'js--ProductCardVerticalPrice__price-current_current-price')
     else:
         current_prices = soup.find_all('span', class_='ProductCardHorizontal__price_current-price')
+
     num_of_pages = soup.find_all('a', class_=(re.compile('PaginationWidget__page js--PaginationWidget__page')))
     cards = list(cards)
     current_prices = list(current_prices)
 
     #page counter
-    if data_page == 1:
-        data_page = len(num_of_pages)
-    else:
-        pass
-    print(f'In {device[device_counter]} citilink have {data_page} pages')
+    int_page = []
+    if page == 1:
+        for each_page in num_of_pages:
+            try:
+                int_page.append(int((each_page.text.strip())))
+            except:
+                pass
+            data_page = max(int_page)
+        print(f'In {device[device_counter]} citilink have {data_page} pages')
 
     card_list = []
     price_list = []
+    link_list = []
 
-    #preparing data to collect
+    # preparing data to collect
     for card in cards:
+        card_link = card.get('href')
         card = card.text.strip()
         card_list.append(card)
+        link_list.append('https://www.citilink.ru' + card_link)
         if device[device_counter] == 'moduli-pamyati':
             card_list.append(card)
+            link_list.append('https://www.citilink.ru' + card_link)
         else:
             pass
     for price in current_prices:
         if device[device_counter] == 'moduli-pamyati':
-            price = price.text.strip()
+            price = int(price.text.strip().replace(' ', ''))
             price_list.append(price)
         else:
-            price = price.text.strip()
+            price = int(price.text.strip().replace(' ', ''))
             price_list.append(price)
 
-    #print progress
+    # print progress
     print(f'{page}/{data_page} pages \nPlease stand by...')
-    print(len(card_list), len(price_list))
+    print(len(card_list), len(price_list), len(link_list))
 
-    #collecting progress in dict and writing to file
-    result = dict(zip(card_list, price_list))
+    #collect info in set
+    result = set(zip(card_list, price_list, link_list))
+    #writing to file.json
     # with open(f' citilinks {device[device_counter]}.json', 'a') as file:
     #     json.dump(result, file, indent=4, ensure_ascii=False)
 
-    for prod, price in result.items():
-        cursor.execute(f'insert into citilink_{now}(product_name, price, class) values (%s, %s, %s)',
-                       (prod, price, device[device_counter]))
-
-
+    #writing to DB
+    for card in result:
+        cursor.execute(f'insert into citilink_{now}(product_name, price, class, link) values (%s, %s, %s, %s)',
+                       (card[0], card[1], device[device_counter], card[2]))
 
 
 def main():
     global page, device_counter, data_page
-
 
     #walking on products and getting data from all pages
     while device_counter != len(device):
@@ -122,6 +131,7 @@ def main():
     connection.commit()
     cursor.close()
     connection.close()
+
 
 if __name__ == '__main__':
     main()
